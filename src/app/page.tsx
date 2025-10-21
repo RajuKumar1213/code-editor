844194c93285e2a30577d047f54d70af148e7308
RajuKumar1213 <rajuvis778@gmail.com>
2025-10-21 15:32:55 +0530

    connected database.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import { Button } from "@/components/ui/button";
+import prisma from "@/lib/prisma";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
   return (
     <div className="flex items-center justify-center h-screen">
       <Button> Get Started </Button>
