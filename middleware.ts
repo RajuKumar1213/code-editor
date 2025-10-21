af18a1b3900df1092395d7d7a2a8e32444f63412
RajuKumar1213 <rajuvis778@gmail.com>
2025-10-21 18:34:14 +0530

    second commit

---
+++ middleware.ts
@@ -0,0 +1,53 @@
+import NextAuth from 'next-auth';
+
+import {
+  DEFAULT_LOGIN_REDIRECTS,
+  apiAuthPrefix,
+  publicRoutes,
+  authRoutes,
+} from './routes';
+
+import authConfig from './auth.config';
+
+const { auth } = NextAuth(authConfig);
+
+export default auth((req) => {
+  const { nextUrl } = req;
+
+  const isLoggedIn = !!req.auth;
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute) {
+    return null;
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECTS, nextUrl));
+    }
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL('/auth/sign-in', nextUrl));
+  }
+
+  return null;
+});
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+  ],
+};
---
+++ routes.ts
@@ -0,0 +1,12 @@
+// * @type {string[]}
+// */
+
+export const publicRoutes: string[] = [];
+
+export const protedtedRoutes: string[] = [];
+
+export const authRoutes: string[] = [];
+
+export const DEFAULT_LOGIN_REDIRECTS: string[] = [];
+
+export const apiAuthPrefix: string[] = [];
